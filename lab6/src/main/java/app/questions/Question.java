dcc44b49e11105b75ab500fd62e74309a0c56f69
Maker <trystenkahtava@gmail.com>
2023-06-19 21:14:54 -0400

    Initial commit

---
+++ lab6/src/main/java/app/questions/Question.java
@@ -0,0 +1,40 @@
+package app.questions;
+
+import java.util.Random;
+
+/**
+ * Represents a multiplication question
+ */
+public interface Question{
+
+    // Used to indicate whether a user's answer was correct, incorrect, or invalid
+    String ANSWER_CORRECT = "correct";
+    String ANSWER_INCORRECT = "incorrect";
+    String ANSWER_INVALID = "invalid";
+
+    // The two operands for the multiplication question
+
+
+    /**
+     * Creates a question with a random pair of operands between 0 and 12
+     * @param rand A random number generator
+     */
+
+
+    /**
+     * @return A prompt to display when 'asking' the question
+     */
+    String getPrompt();
+
+    /**
+     * @return A message indicating the expected value type for answers
+     */
+    String getValidityMessage();
+
+    /**
+     * Parse a given answer and determine whether it is correct, incorrect, or invalid
+     * @param answer The answer to parse
+     * @return One of ANSWER_CORRECT, ANSWER_INCORRECT, or ANSWER_INVALID
+     */
+    String parseAnswer(String answer);
+}
---
+++ lab6/src/main/java/data/DataService.java
@@ -0,0 +1,14 @@
+package data;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class DataService {
+
+    /**
+     * @return An array of MultipleChoiceQuestions based on the content of the math-questions.txt file
+     */
+
+}
